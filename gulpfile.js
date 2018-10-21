bf2fa0a95bcdaf3a388d11e0e0eb1943db239f1f
Brad Pitcher <bradpitcher@gmail.com>
2018-10-20 22:30:43 -0600

    customize gulpfile.js to our liking

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,12 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var cache = require('gulp-cache');
 var cp = require('child_process');
-var browserSync = require('browser-sync');
 
 
-// Build the Jekyll Site
-gulp.task('jekyll-build', function (done) {
-    return cp.spawn('bundle' , ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+// Serve the Jekyll Site
+gulp.task('jekyll-serve', function (done) {
+    return cp.spawn('bundle' , ['exec', 'jekyll', 'serve'], {stdio: 'inherit'})
         .on('close', done);
-});
-
-// Rebuild Jekyll and page reload
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-// Wait for jekyll-build, then launch the Server
-gulp.task('browser-sync', ['sass', 'img', 'fonts', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        },
-        notify: false
-    });
 });
 
 // Compile files
@@ -44,13 +28,13 @@ gulp.task('sass', function () {
 
 // Compression images
 gulp.task('img', function() {
-	return gulp.src('assets/img/**/*')
-		.pipe(cache(imagemin({
-			interlaced: true,
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant()]
-		})))
+  return gulp.src('assets/img/**/*')
+    .pipe(cache(imagemin({
+      interlaced: true,
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    })))
     .pipe(gulp.dest('_site/assets/img'))
     .pipe(browserSync.reload({stream:true}));
 });
@@ -65,11 +49,9 @@ gulp.task('fonts', function() {
 // Watch scss, html, img files
 gulp.task('watch', function () {
     gulp.watch('assets/css/sass/*.*', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['jekyll-rebuild']);
     gulp.watch('assets/img/**/*', ['img']);
-    gulp.watch('assets/fonts/**/*', ['fonts']);    
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/fonts/**/*', ['fonts']);
 });
 
 //  Default task
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['jekyll-serve', 'watch']);
