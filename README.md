1bf283d9395b2307ff957ec66366700eca58cb67
Brad Pitcher <bradpitcher@gmail.com>
2018-10-27 15:41:20 -0700

    switch to nisarg (WIP) theme

---
+++ README.md
@@ -0,0 +1,46 @@
+== LICENSES ==
+
+All content is copyright Wanderland Travelers 2016-2018
+
+Nisarg WordPress Theme, Copyright 2015-2016 Falguni Desai
+Nisarg is distributed under the terms of the GNU GPL
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For copy of GNU General Public License see: (http://www.gnu.org/licenses/ ).
+----------------------------------------------------------------------------------------------------------------
+
+Bootstrap - http://getbootstrap.com/
+Licensed Under MIT( https://opensource.org/licenses/MIT)
+----------------------------------------------------------------------------------------------------------------
+html5shiv - http://code.google.com/p/html5shiv/
+License Under MIT (http://opensource.org/licenses/MIT)
+-----------------------------------------------------------------------------------------------------------------
+Font Awesome - http://fortawesome.github.io/Font-Awesome/
+
+Font License
+
+    Applies to all desktop and webfont files in the following directory: font-awesome/font/.
+    License: SIL OFL 1.1
+    URL: http://scripts.sil.org/OFL
+
+Code License
+
+    Applies to all CSS and LESS files in the following directories: font-awesome/css/ and font-awesome/less/.
+    License: MIT License
+    URL: http://opensource.org/licenses/mit-license.html
+
+-----------------------------------------------------------------------------------------------------------------
+Google Fonts - http://www.google.com/fonts/
+Licensed Under: SIL Open Font License, 1.1 (http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL)
+"Lato" - SIL Open Font License, 1.1 
+"Source Sans Pro" -	SIL Open Font License, 1.1 
+------------------------------------------------------------------------------------------------------------------
--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,118 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var fs = require('fs');
+var merge = require('merge-stream');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var cache = require('gulp-cache');
 var cp = require('child_process');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
-// Serve the Jekyll Site
-gulp.task('jekyll-serve', function (done) {
-    return cp.spawn('bundle' , ['exec', 'jekyll', 'serve'], {stdio: 'inherit'})
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('bundle', ['exec', jekyll, 'build'], { stdio: 'inherit' })
         .on('close', done);
 });
 
-// Compile files
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build', 'fonts', 'sass', 'js'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['jekyll-build', 'fonts', 'sass', 'js'], function () {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+gulp.task('clean:fonts', function () {
+    return gulp.src('_site/assets/fonts').pipe(clean({ force: true }));
+});
+
+gulp.task('fonts', ['clean:fonts', 'sass'], function () {
+    if (!fs.existsSync('_site/assets/fonts')) {
+        fs.mkdirSync('_site/assets/fonts');
+    }
+    return gulp.src([
+        './node_modules/font-awesome/fonts/*',
+    ]).pipe(gulp.dest('_site/assets/fonts/'));
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
 gulp.task('sass', function () {
-    return gulp.src('assets/css/sass/main.sass')
+    var sassStream,
+    cssStream;
+
+    //compile sass
+    sassStream = gulp.src('src/scss/style.scss')
         .pipe(sass({
-            outputStyle: 'expanded',
+            includePaths: ['scss'],
             onError: browserSync.notify
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(prefix(['last 3 versions'], { cascade: true }));
+
+    //select additional css files
+    cssStream = gulp.src([
+        './node_modules/font-awesome/css/font-awesome.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+    ]);
+
+    //merge the two streams and concatenate their contents into a single file
+    return merge(cssStream, sassStream)
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-// Compression images
-gulp.task('img', function() {
-  return gulp.src('assets/img/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    })))
-    .pipe(gulp.dest('_site/assets/img'))
-    .pipe(browserSync.reload({stream:true}));
+gulp.task('js', function () {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/bootstrap.js',
+        './node_modules/html5shiv/dist/html5shiv.js',
+    ].concat(['src/js/*']))
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(uglify({ mangle: false }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('_site/assets/js'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-// Fonts
-gulp.task('fonts', function() {
-    return gulp.src('assets/fonts/**/*')
-        .pipe(gulp.dest('_site/assets/fonts'))
-        .pipe(browserSync.reload({stream:true}));
-});
-
-// Watch scss, html, img files
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/sass/*.*', ['sass']);
-    gulp.watch('assets/img/**/*', ['img']);
-    gulp.watch('assets/fonts/**/*', ['fonts']);
+    gulp.watch(['src/scss/*.scss', 'src/scss/*/*.scss'], ['sass']);
+    gulp.watch(['src/js/*.js'], ['js']);
+    gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
-//  Default task
-gulp.task('default', ['jekyll-serve', 'watch']);
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
