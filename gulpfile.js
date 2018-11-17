73560f21fc501782397a3caeb6f29bd2c0980fc2
Brad Pitcher <bradpitcher@gmail.com>
2018-11-16 21:23:37 -0800

    fix some font stylings

--- gulpfile.js
+++ gulpfile.js
@@ -130,4 +130,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browserSync', 'watch']);
+gulp.task('default', ['watch', 'browserSync']);
